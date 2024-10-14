3da0ccbf21e797192b6275d1949a8a039a89baf8
nishant <nishantpacharne16@gmail.com>
2024-10-14 09:52:49 +0530

    contract ready

---
+++ assembly/Constants.ts
@@ -0,0 +1,15 @@
+import { Base58 } from "@koinos/sdk-as";
+
+export namespace Constants {
+  export const NAME: string = "RealEstateRun LOT1";
+  export const SYMBOL: string = "RER1";
+  export const MINT_PRICE: u64 = 3000000000000;
+  export const MINT_FEE: bool = true;
+  export const MAX_SUPPLY: u64 = 250;
+  export const URI: string = "https://ipfs.filebase.io/ipfs/QmWgBi2pqkSZGRf4kqWQgAUdtFFyxvSXzQmFdw4d1R3kvz/";
+  export const OWNER: Uint8Array = Base58.decode("1NPvJuEDhjLuW3VbGqAQe4MdSWSB3YWxK1");
+
+  // token mint
+  export const TOKEN_PAY: Uint8Array = Base58.decode("18JRrBdnNqQ99faV6sn6Un1MbvU5sZWgzf");
+  export const ADDRESS_PAY: Uint8Array = Base58.decode("1CounterpartyXXXXXXXXXXXXXXXUWLpVr");
+}
